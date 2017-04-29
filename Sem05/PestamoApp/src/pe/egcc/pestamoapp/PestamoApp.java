0ba0c4994a39335d4346379d40f6c25241647108
Gustavo Coronel <gcoronelc@gmail.com>
2017-04-29 08:58:02 -0500

    Sem05

---
+++ Sem05/PestamoApp/src/pe/egcc/pestamoapp/PestamoApp.java
@@ -0,0 +1,11 @@
+package pe.egcc.pestamoapp;
+
+import pe.egcc.pestamoapp.view.PrestamoView;
+
+public class PestamoApp {
+
+  public static void main(String[] args) {
+    PrestamoView.main(args);
+  }
+  
+}
